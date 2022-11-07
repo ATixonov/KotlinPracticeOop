9ce6648c5aabd1674a664133c0ea59ddb259a6aa
ATixonov <tixxxonnnofff@rambler.ru>
2022-11-07 22:11:52 +0300

    next lesson

--- app/src/main/java/ru/synergy/kotlinpracticeoop/MainActivity.kt
+++ app/src/main/java/ru/synergy/kotlinpracticeoop/MainActivity.kt
@@ -13,6 +13,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        val tom = createPerson("tom", "jetbread")
+//        tom.sayHello()
+
+//        hello(object : Person("sam", "1234567"){
+//            val company = "jetbrains"
+//            override fun sayHello() {
+//                println("hi my name is $username i work in $company")
+//            }
+//        })
+
+//        val tom = object: Person("tom", "123456") {
+//            val company = "jetbrains"
+//            val age = 37
+//            fun sayHello() {
+//                println("hi my name is $username")
+//            }
+//        }
+//        println("name: ${tom.username} age: ${tom.age}")
+//        tom.sayHello()
+
+//        val userAcc: Person = Person("qwerty", "1234")
+//        userAcc.showDetails()
+//
+//        val acc = BankAccount(100000)
+//        acc.Transaction(2400).pay()
+
 //        val kitty: Human = Human("kitty", 19)
 //        var (username, userage) = kitty
 
@@ -21,4 +47,15 @@ class MainActivity : AppCompatActivity() {
         val mSms = smsMessage("hello sms kotlin")
         sendSms(mSms, "+777")
     }
+
+//    fun hello (person: Person){
+//        person.sayHello()
+//    }
+
+//    private fun createPerson(_name: String, _company: String) = object {
+//        val name = _name
+//        val company = _company
+//        fun sayHello() = println("hi my name is $name i work in $company")
+//    }
+
 }
--- app/src/main/java/ru/synergy/kotlinpracticeoop/Person.kt
+++ app/src/main/java/ru/synergy/kotlinpracticeoop/Person.kt
@@ -1,30 +1,42 @@
 package ru.synergy.kotlinpracticeoop
 
-open class Person(val name: String) {
+open class Person(val username: String, val password: String) {
 
-    fun printName(){
-        print(name)
+    private val account: Account = Account(username, password)
+
+    private class Account(val username: String, val password: String){
+
+    }
+    fun showDetails(){
+        println("username: $username password: $password")
+    }
+
+    open fun sayHello() {
+        println("hi my name is $username")
     }
 }
 
-class Employee (empName: String, private var company: String): Person(empName) {
-
+/*class Employee (empName: String, private var company: String): Person(empName) {
     private var age: Int = 10
     set(value: Int){
         if ((value>0) and (value<110))
             field = value        // field=age
     }
-
-
     val fullInformation: String
-    get() = "$name $age $company"
-
-
+    get() = "$age $company"
     fun printCompany() {
         println(company)
+    }*/
 
-
+class BankAccount(private var sum: Int){
+    fun display(){
+        println("sum = $sum")
     }
-
+    inner class Transaction(private val sum: Int) {
+        fun pay() {
+            this@BankAccount.sum -= this@Transaction.sum
+            display()
+        }
+    }
 }
 
